e4bae65f40633ebe31a3212beaafc75a1d5ef43f
Russell Johnson <encrypteddata0@gmail.com>
2023-12-19 14:01:15 -0800

    [ItemCategory] POST now does update and uses JSON instead of URL-Form-Encoding

--- SuperdarkLists.Test.UnitTests/Controllers/ItemCategories/ItemCategoriesPostTests.cs
+++ SuperdarkLists.Test.UnitTests/Controllers/ItemCategories/ItemCategoriesPostTests.cs
@@ -1,8 +1,9 @@
 using FluentAssertions;
 using Microsoft.AspNetCore.Mvc;
 using SuperdarkLists.Api.v1;
-using SuperdarkLists.DomainModel.Database.Model;
 using SuperdarkLists.DomainModel.Rest.ErrorModel;
+using SuperdarkLists.DomainModel.Rest.Model;
+using ItemCategory = SuperdarkLists.DomainModel.Database.Model.ItemCategory;
 
 namespace SuperdarkLists.Test.UnitTests.Controllers.ItemCategories;
 
@@ -11,15 +12,15 @@ namespace SuperdarkLists.Test.UnitTests.Controllers.ItemCategories;
 public class ItemCategoriesPostTests : ItemCategoryControllerTestBase
 {
     [Test]
-    public async Task EmptyNameInBodyReturnsBadRequest()
+    public async Task EmptyNameInBodyReturnsBadRequestWhenCreating()
     {
         // Passing in string.Empty
-        ItemCategoryFormData form = new()
+        ItemCategoryRequest request = new()
         {
             Name = string.Empty
         };
 
-        var result = await UnitUnderTest.PostAsync(form);
+        var result = await UnitUnderTest.PostAsync(request);
 
         // Should give us a result
         result.Should().NotBeNull();
@@ -32,18 +33,73 @@ public class ItemCategoriesPostTests : ItemCategoryControllerTestBase
         // Also the database shouldn't have been saved
         _mockDatabaseContext.SaveChangesCalled.Should().BeFalse();
     }
+
+    [Test]
+    public async Task EmptyNameReturnsBadRequestWhenUpdating()
+    {
+        Guid existingId = Guid.NewGuid();
+        const string existingName = "ItemCategory A";
+        
+        // Add an existing item into our Database
+        _mockDatabaseContext.BackingItemCategories.Add(new ItemCategory()
+        {
+            CategoryId = existingId,
+            Name = existingName
+        });
+        
+        // Try to update with an empty name
+        ItemCategoryRequest request = new()
+        {
+            Id = existingId,
+            Name = string.Empty,
+        };
+
+        var result = await UnitUnderTest.PostAsync(request);
+
+        result.Should().BeOfType<BadRequestObjectResult>()
+            .Subject.Value.Should().BeOfType<Error>()
+            .Subject.ErrorCode.Should().Be(ErrorCode.InvalidOrMissingParameter);
+    }
+
+    [TestCase(null)]
+    [TestCase("")]
+    public async Task MissingNameInUpdateRequestReturnsBadRequest(string name)
+    {
+        Guid existingId = Guid.NewGuid();
+        const string existingName = "ItemCategory A";
+        
+        // Add an existing item into our Database
+        _mockDatabaseContext.BackingItemCategories.Add(new ItemCategory()
+        {
+            CategoryId = existingId,
+            Name = existingName
+        });
+        
+        // Try to update with an empty name
+        ItemCategoryRequest request = new()
+        {
+            Id = existingId,
+            Name = name,
+        };
+
+        var result = await UnitUnderTest.PostAsync(request);
+
+        result.Should().BeOfType<BadRequestObjectResult>()
+            .Subject.Value.Should().BeOfType<Error>()
+            .Subject.ErrorCode.Should().Be(ErrorCode.InvalidOrMissingParameter);
+    }
     
     [TestCase(null)]
     [TestCase("")]
-    public async Task MissingNameInBodyReturnsBadRequest(string name)
+    public async Task MissingNameInBodyReturnsBadRequestWhenCreating(string name)
     {
         // Null name or empty string
-        ItemCategoryFormData form = new()
+        ItemCategoryRequest request = new()
         {
             Name = name
         };
 
-        var result = await UnitUnderTest.PostAsync(form);
+        var result = await UnitUnderTest.PostAsync(request);
 
         // Should yield an result
         result.Should().NotBeNull();
@@ -58,7 +114,7 @@ public class ItemCategoriesPostTests : ItemCategoryControllerTestBase
     }
 
     [Test]
-    public async Task MatchingNameInDatabaseYieldsBadRequest()
+    public async Task MatchingNameInDatabaseYieldsBadRequestWhenCreating()
     {
         const string existingName = "TestItemCategory";
         
@@ -70,12 +126,12 @@ public class ItemCategoriesPostTests : ItemCategoryControllerTestBase
         });
 
         // Same name here
-        ItemCategoryFormData form = new()
+        ItemCategoryRequest request = new()
         {
             Name = existingName
         };
 
-        var result = await UnitUnderTest.PostAsync(form);
+        var result = await UnitUnderTest.PostAsync(request);
 
         // Should return an BadRequest with our Error result and a code of EntityAlreadyExists
         result.Should().BeOfType<BadRequestObjectResult>()
@@ -87,17 +143,17 @@ public class ItemCategoriesPostTests : ItemCategoryControllerTestBase
     }
 
     [Test]
-    public async Task Works()
+    public async Task CreatingNewItemCategoryWorks()
     {
         // We are going to create a new ItemCategory with this name
         const string testName = "TestName";
 
-        ItemCategoryFormData form = new()
+        ItemCategoryRequest request = new()
         {
             Name = testName
         };
 
-        var result = await UnitUnderTest.PostAsync(form);
+        var result = await UnitUnderTest.PostAsync(request);
 
         // We should have created something
         result.Should().BeOfType<CreatedResult>();
@@ -108,6 +164,80 @@ public class ItemCategoriesPostTests : ItemCategoryControllerTestBase
         // Inside our Database we should have our new item
         _mockDatabaseContext.BackingItemCategories.Should().HaveCount(1)
             .And.OnlyContain(e => e.Name == testName);
+    }
+    
+    [Test]
+    public async Task ExistingNameInDatabasePreventsUpdate()
+    {
+        Guid existingCategoryId = Guid.NewGuid();
+        const string existingName = "TestCategory";
+        
+        // Add an ItemCategory to our database
+        _mockDatabaseContext.BackingItemCategories.Add(new ItemCategory()
+        {
+            CategoryId = existingCategoryId,
+            Name = existingName
+        });
+        
+        // We want to update an item using the existing name
+        const string newName = existingName;
+        
+        // Create the request using the existing Id which will update our ItemCategory
+        ItemCategoryRequest request = new()
+        {
+            Id = existingCategoryId,
+            Name = newName
+        };
+
+        var result = await UnitUnderTest.PostAsync(request);
+
+        // Result should be correct
+        result.Should().BeOfType<BadRequestObjectResult>()
+            .Which.Value.Should().BeOfType<Error>()
+            .Which.ErrorCode.Should().Be(ErrorCode.InvalidOrMissingParameter);
+        
+        // Our Database should not be modified
+        _mockDatabaseContext.SaveChangesCalled.Should().BeFalse();
+    }
+
+    [Test]
+    public async Task UpdatingExitingItemCategoryWorks()
+    {
+        Guid existingCategoryId = Guid.NewGuid();
+        const string existingName = "TestCategory";
+        
+        // Add an ItemCategory to our database
+        _mockDatabaseContext.BackingItemCategories.Add(new ItemCategory()
+        {
+            CategoryId = existingCategoryId,
+            Name = existingName
+        });
+        
+        // We want to update the item to a new name
+        const string newName = "New TestCategory Name";
+        
+        // Create the request using the existing Id which will update our ItemCateogry
+        ItemCategoryRequest request = new()
+        {
+            Id = existingCategoryId,
+            Name = newName
+        };
+
+        var result = await UnitUnderTest.PostAsync(request);
+
+        // Result should be correct
+        result.Should().BeOfType<OkObjectResult>()
+            .Which.Value.Should().BeOfType<DomainModel.Rest.Model.ItemCategory>()
+            .Subject.Should().BeEquivalentTo(new DomainModel.Rest.Model.ItemCategory()
+            {
+                Id = existingCategoryId,
+                Name = newName
+            });
+        
+        // Our database should be updated
+        _mockDatabaseContext.BackingItemCategories.Should()
+            .OnlyContain(e => e.CategoryId == existingCategoryId && e.Name == newName);
+        _mockDatabaseContext.SaveChangesCalled.Should().BeTrue();
     }
     
 }
